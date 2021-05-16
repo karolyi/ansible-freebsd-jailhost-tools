80f352d8570c7f18062cd4f3216bf9684280c80f
László Károlyi <laszlo@karolyi.hu>
2021-05-16 17:00:20 +0200

    Adjusting revalidator due to changed pkg store format

--- tools/pkgmirror-jailrevalidator.py
+++ tools/pkgmirror-jailrevalidator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 
 from argparse import ArgumentParser
+from collections import defaultdict
 from hashlib import sha256
 from http.client import HTTPResponse
 from io import BytesIO
@@ -44,47 +45,70 @@ def _revalidate_packagesite(abi: str, pkgmirror_url: str) -> List[bytes]:
     return exfile.read().splitlines()
 
 
-def _load_packages(lines: List[bytes]) -> dict:
+def _load_packages(lines: List[bytes]) -> defaultdict:
     """
     Load and return the packages from the passed JSON structured lines.
     """
-    result = dict()
+    result = defaultdict(dict)
     for line in lines:
+        # print(f'{line}\n')
         loaded = loads(line)
         name = loaded['name']
-        if name in result:
-            raise KeyError(f'{name} is defined twice in packages.')
-        result[name] = loaded
+        version = loaded['version']
+        result[name][version] = loaded
     return result
 
 
 def _extract_deps(
-        packages_list: dict, passed_packages: List[str]) -> Set[str]:
+        infodict_packages: defaultdict, passed_packages: dict) -> dict:
     'Compile and return the packages to check, including dependencies.'
-    result = set(passed_packages)
-    for package in passed_packages:
-        if 'deps' not in packages_list[package]:
-            continue
-        result.update(_extract_deps(
-            packages_list=packages_list,
-            passed_packages=list(packages_list[package]['deps'])))
+    result = dict()
+    for name, versions in passed_packages.items():
+        for version in versions:
+            dict_version = infodict_packages[name][version]
+            if 'deps' not in dict_version:
+                continue
+            for depended_pkg, dict_depended_item in \
+                    dict_version['deps'].items():
+                if depended_pkg not in result:
+                    result[depended_pkg] = set()
+                result[depended_pkg].add(dict_depended_item['version'])
+    if not result:
+        return result
+    dict_deps = _extract_deps(
+        infodict_packages=infodict_packages, passed_packages=result)
+    for name, versions in dict_deps.items():
+        if name not in result:
+            result[name] = set()
+        result[name].update(versions)
     return result
 
 
 def _get_packages_to_check(
-    pkgmirror_url: str, abi: str, packages_list: dict,
-    passed_packages: List[str]
-) -> dict:
+    pkgmirror_url: str, abi: str, infodict_packages: defaultdict,
+    passed_packages: Set[str],
+) -> List[dict]:
     'Compile and return the packages to check.'
-    to_check = _extract_deps(
-        packages_list=packages_list, passed_packages=passed_packages)
+    set_not_in_packages = passed_packages - set(infodict_packages)
+    if set_not_in_packages:
+        raise KeyError(f'Packages not found: {set_not_in_packages}')
+    dict_pkgs = {
+        name: set(infodict_packages[name]) for name in passed_packages}
+    dict_deps = _extract_deps(
+        infodict_packages=infodict_packages, passed_packages=dict_pkgs)
+    for name, versions in dict_deps.items():
+        if name not in dict_pkgs:
+            dict_pkgs[name] = set()
+        dict_pkgs[name].update(versions)
     url_prefix = '/'.join((pkgmirror_url, abi, 'latest'))
-    result = dict()
-    for name in to_check:
-        info = packages_list[name]
-        result[name] = dict(
-            url='/'.join((url_prefix, info['repopath'])),
-            pkgsize=info['pkgsize'], sha256=info['sum'])
+    result = list()
+    for name, versions in dict_pkgs.items():
+        for version in versions:
+            dict_version = infodict_packages[name][version]
+            result.append(dict(
+                name=name, version=version,
+                url='/'.join((url_prefix, dict_version['repopath'])),
+                pkgsize=dict_version['pkgsize'], sha256=dict_version['sum']))
     return result
 
 
@@ -97,42 +121,44 @@ def _fetch_and_get_info(request: Request) -> dict:
     return dict(size=len(content), digest=hasher.hexdigest())
 
 
-def _get_to_revalidate(packages_to_check: dict) -> dict:
+def _get_to_revalidate(packages_to_check: List[dict]) -> List[dict]:
     """
     Download the packages in the dict return the mismatched ones in a
     new `dict`.
     """
     to_revalidate = dict()
-    for name, info in packages_to_check.items():  # type: str, dict
-        url = info['url']
+    for dict_info in packages_to_check:
+        name = dict_info['name']
+        url = dict_info['url']
         request = Request(url=url)
         dl_info = _fetch_and_get_info(request=request)
-        if info['pkgsize'] != dl_info['size']:
+        if dict_info['pkgsize'] != dl_info['size']:
             print(f'Size mismatch: {name}')
-            to_revalidate[name] = info
+            to_revalidate.append(dict_info)
             continue
-        if info['sha256'] != dl_info['digest']:
+        if dict_info['sha256'] != dl_info['digest']:
             print(f'SHA256 sum mismatch: {name}')
-            to_revalidate[name] = info
+            to_revalidate.append(dict_info)
             continue
         print(f'OK: {name}')
     return to_revalidate
 
 
-def _revalidate_packages(to_revalidate: dict) -> bool:
+def _revalidate_packages(to_revalidate: List[dict]) -> bool:
     'Revalidate the packages that are mismatched.'
     headers = {'Cache-Bypass': 'true'}
     success = True
-    for name, info in to_revalidate.items():  # type: str, dict
+    for dict_item in to_revalidate:
+        name = dict_item['name']
+        url = dict_item['url']
         print(f'Revalidating {name} ... ', end='')
-        url = info['url']
         request = Request(url=url, headers=headers)
         dl_info = _fetch_and_get_info(request=request)
-        if info['pkgsize'] != dl_info['size']:
+        if dict_item['pkgsize'] != dl_info['size']:
             print('Size mismatch!')
             success = False
             continue
-        if info['sha256'] != dl_info['digest']:
+        if dict_item['sha256'] != dl_info['digest']:
             print('SHA256 sum mismatch!')
             success = False
             continue
@@ -151,14 +177,15 @@ def run():
     path_jailroot = Path(args.jail_root)
     if not path_jailroot.is_dir():
         raise FileNotFoundError(path_jailroot)
+    passed_packages = set(args.packages.split())
     abi = _get_abi(jail_root=args.jail_root)
     _check_pkgmirror_url(url=args.pkgmirror_url)
     lines = _revalidate_packagesite(abi=abi, pkgmirror_url=args.pkgmirror_url)
-    packages_list = _load_packages(lines=lines)
-    passed_packages = args.packages.split()
+    infodict_packages = _load_packages(lines=lines)
     packages_to_check = _get_packages_to_check(
-        pkgmirror_url=args.pkgmirror_url, abi=abi, packages_list=packages_list,
-	passed_packages=passed_packages)
+        pkgmirror_url=args.pkgmirror_url, abi=abi,
+        infodict_packages=infodict_packages,
+        passed_packages=passed_packages)
     to_revalidate = _get_to_revalidate(packages_to_check=packages_to_check)
     if to_revalidate:
         if not _revalidate_packages(to_revalidate=to_revalidate):
